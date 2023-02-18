dd297f9196c8695d4415fda22a9618bbca4f1f1b
algnot <thanawat.k2000@gmail.com>
2023-02-18 14:02:02 +0700

    add jquery

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,4 @@
 import '../styles/globals.css'
-import '../styles/owl.carousel.min.css'
-import '../styles/owl.theme.default.min.css'
 import '../styles/unicons.css'
 import '../styles/tooplate-style.css'
 import Head from 'next/head'
@@ -27,16 +25,15 @@ export default function MyApp({ Component, pageProps }) {
                 <link href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossOrigin="anonymous"/> 
                 <link href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossOrigin="anonymous"/> 
                 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossOrigin="anonymous"/> 
-                <script src="http://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js" type="text/javascript"/><script/>
             </Head>
             <Navbar />
             <Component {...pageProps} />
             <Footer />
             <Script id="bootstrap-cdn" src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" />
+            <Script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"/>
             <Script src="/js/jQuery.headroom.js"/>
             <Script src="/js/popper.min.js"/>
             <Script src="/js/Headroom.js"/>
-            <Script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"/>
             <Script src="/js/smoothscroll.js"/>
             <Script src="/js/custom.js"/>
         </>
