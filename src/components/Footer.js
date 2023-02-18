472634eb2705c4dbc02b9b9b0845584c03fe2cab
algnot <thanawat.k2000@gmail.com>
2023-02-18 12:17:35 +0700

    add role

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,19 +2,13 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer class="footer py-5">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-12 col-12">
-            <p class="copyright-text text-center">
-              Copyright &copy; 2023 Tongla.
+    <footer className="footer py-5">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-12 col-12">
+            <p className="copyright-text text-center">
+              Copyright © 2023 Tongla.
             </p>
-            {/* <p class="copyright-text text-center">
-              Designed by{" "}
-              <a rel="nofollow" href="https://www.facebook.com/tooplate">
-                Tooplate
-              </a>
-            </p> */}
           </div>
         </div>
       </div>
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,9 +5,10 @@ import '../styles/owl.theme.default.min.css'
 import '../styles/unicons.css'
 import '../styles/tooplate-style.css'
 import Head from 'next/head'
-import Script from 'next/script'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import Script from 'next/script'
+
 
 export default function MyApp({ Component, pageProps }) {
     return (
@@ -16,20 +17,20 @@ export default function MyApp({ Component, pageProps }) {
                 <title>Tongla site :)</title>
                 <meta charset="utf-8"/>
                 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
-                <meta name="description" content="Tongla site :)"/>
+                <meta name="description" content="Tongla site"/>
                 <meta name="author" content="Thanawat Talabtong"/>
             </Head>
-            <Script src="js/jquery-3.3.1.min.js"/>
-            <Script src="js/popper.min.js"/>
-            <Script src="js/bootstrap.min.js"/>
-            <Script src="js/Headroom.js"/>
-            <Script src="js/jQuery.headroom.js"/>
-            <Script src="js/owl.carousel.min.js"/>
-            <Script src="js/smoothscroll.js"/>
-            <Script src="js/custom.js"/>
             <Navbar />
             <Component {...pageProps} />
             <Footer />
+            <Script src="/js/jquery-3.3.1.min.js"/>
+            <Script src="/js/jQuery.headroom.js"/>
+            <Script src="/js/popper.min.js"/>
+            <Script src="/js/bootstrap.min.js"/>
+            <Script src="/js/Headroom.js"/>
+            <Script src="/js/owl.carousel.min.js"/>
+            <Script src="/js/smoothscroll.js"/>
+            <Script src="/js/custom.js"/>
         </>
     )
 }
