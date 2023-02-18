960637bab216d0e291ab6e391d06990be17d2c16
algnot <thanawat.k2000@gmail.com>
2023-02-18 13:50:17 +0700

    add jquery

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -7,9 +7,15 @@ import Head from 'next/head'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import Script from 'next/script'
+import { useEffect } from 'react'
 
 
 export default function MyApp({ Component, pageProps }) {
+
+    useEffect(() => {
+        window.$ = window.jQuery = require('jquery')
+    }, [])
+
     return (
         <>
             <Head>
@@ -19,12 +25,11 @@ export default function MyApp({ Component, pageProps }) {
                 <meta name="description" content="Tongla site"/>
                 <meta name="author" content="Thanawat Talabtong"/>   
                 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossOrigin="anonymous"/> 
-                <script src="http://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js" type="text/javascript"/><script/>
+                {/* <script src="http://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js" type="text/javascript"/><script/> */}
             </Head>
             <Navbar />
             <Component {...pageProps} />
             <Footer />
-            <Script src="js/jquery-3.3.1.min" type="text/javascript"/>
             <Script id="bootstrap-cdn" src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" />
             <Script src="/js/jQuery.headroom.js"/>
             <Script src="/js/popper.min.js"/>
