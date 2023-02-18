4e1d98bdc352fb7ead30f4eb637cb30aec867004
algnot <thanawat.k2000@gmail.com>
2023-02-18 13:54:52 +0700

    add jquery

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -25,7 +25,7 @@ export default function MyApp({ Component, pageProps }) {
                 <meta name="description" content="Tongla site"/>
                 <meta name="author" content="Thanawat Talabtong"/>   
                 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossOrigin="anonymous"/> 
-                {/* <script src="http://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js" type="text/javascript"/><script/> */}
+                <script src="http://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js" type="text/javascript"/><script/>
             </Head>
             <Navbar />
             <Component {...pageProps} />
