5fbb8196d618c545654353fc0feb3143d54f97a8
algnot <thanawat.k2000@gmail.com>
2023-02-18 14:00:37 +0700

    add jquery

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -24,6 +24,8 @@ export default function MyApp({ Component, pageProps }) {
                 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
                 <meta name="description" content="Tongla site"/>
                 <meta name="author" content="Thanawat Talabtong"/>   
+                <link href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossOrigin="anonymous"/> 
+                <link href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossOrigin="anonymous"/> 
                 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossOrigin="anonymous"/> 
                 <script src="http://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js" type="text/javascript"/><script/>
             </Head>
@@ -34,7 +36,7 @@ export default function MyApp({ Component, pageProps }) {
             <Script src="/js/jQuery.headroom.js"/>
             <Script src="/js/popper.min.js"/>
             <Script src="/js/Headroom.js"/>
-            <Script src="/js/owl.carousel.min.js"/>
+            <Script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"/>
             <Script src="/js/smoothscroll.js"/>
             <Script src="/js/custom.js"/>
         </>
