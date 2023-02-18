b3185054872fbfe9a855518531d5e8722f5ed3bb
algnot <thanawat.k2000@gmail.com>
2023-02-18 12:36:35 +0700

    fix web

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,5 @@
 import '../styles/globals.css'
-import '../styles/bootstrap.min.css'
+// import '../styles/bootstrap.min.css'
 import '../styles/owl.carousel.min.css'
 import '../styles/owl.theme.default.min.css'
 import '../styles/unicons.css'
@@ -18,15 +18,16 @@ export default function MyApp({ Component, pageProps }) {
                 <meta charset="utf-8"/>
                 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
                 <meta name="description" content="Tongla site"/>
-                <meta name="author" content="Thanawat Talabtong"/>
+                <meta name="author" content="Thanawat Talabtong"/>   
+                <link    href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossOrigin="anonymous"/>    
             </Head>
             <Navbar />
             <Component {...pageProps} />
             <Footer />
-            <Script src="/js/jquery-3.3.1.min.js"/>
+            <Script src="http://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js" type="text/javascript"/>
+            <Script id="bootstrap-cdn" src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" />
             <Script src="/js/jQuery.headroom.js"/>
             <Script src="/js/popper.min.js"/>
-            <Script src="/js/bootstrap.min.js"/>
             <Script src="/js/Headroom.js"/>
             <Script src="/js/owl.carousel.min.js"/>
             <Script src="/js/smoothscroll.js"/>
